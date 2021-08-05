7daa223d7322c742bf7fedc6d6d59f483713cb86
aeonincode <khashayar.sasannia@hotmail.com>
2021-08-05 16:57:17 +0200

    add more structures functions

--- server.go
+++ server.go
@@ -2,7 +2,33 @@ package main
 
 import "net/http"
 
+// define data type so we can return something
+type Coaster struct {
+	Name         string `json:"name"`
+	Manufacturer string `json:"manufacturer"`
+	ID           string `json:"id"`
+	InPark       string `json:"inPark"`
+	Height       int    `json:"height"`
+}
+
+type coasterHandlers struct {
+	store map[string]Coaster
+}
+
+// use h for handler
+func (h *coasterHandlers) get(w http.ResponseWriter, r *http.Request) {
+}
+
+// new new constructer function to create this thing
+func newCoasterHandlers() *coasterHandlers {
+	return &coasterHandlers{
+		store: map[string]Coaster{},
+	}
+}
+
 func main() {
+	coasterHandlers := newCoasterHandlers()
+	http.HandleFunc("/coasters", coasterHandlers.get)
 	err := http.ListenAndServe(":8080", nil)
 	if err != nil {
 		panic(err)
